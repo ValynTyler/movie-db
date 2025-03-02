7311a309e43c36441795d0f2e560fdb219e1cac1
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-02 21:59:13 +0200

    feat: fetching movies

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-id=64
 
-./api/actor_movies.sh $id \
-  | jq -r '.cast 
-    | sort_by(.release_date)
-    | reverse[0]
-    | "This actor'\''s latest movie is \(.title)"' \
-  | bat
+./api/actors.sh \
+| jq -r '.results[].id' \
+| while read id; do
+  ./api/actor_movies.sh $id \
+    | jq -r '.cast 
+      | sort_by(.release_date)
+      | reverse[0]
+      | "\(.release_date): \(.title)"'
+done
