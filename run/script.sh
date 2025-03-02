8c5d5d72277dde5f54080ada6ef6cd3a54cc06c0
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-02 19:21:28 +0200

    feat: fetching actor movies

--- run/script.sh
+++ run/script.sh
@@ -13,13 +13,26 @@ if ! grep -q "^API_KEY=" ".env"; then
   echo "API_KEY=$(get_api_key)" >> .env
 fi
 
-get_actors() {
+get_actor_ids() {
   curl \
     -X GET "https://api.themoviedb.org/3/person/popular?api_key=$API_KEY" \
     -H "Accept: application/json" \
-    | jq -r '.results[].name'
+    | jq -r '.results[].id'
 }
 
-get_actors | while read actor; do
-  echo "$actor is an actor."
+get_name_by_id() {
+  curl -s "https://api.themoviedb.org/3/person/$1?api_key=$API_KEY" | jq -r '.name'
+}
+
+get_latest_movie() {
+  curl \
+    -s "https://api.themoviedb.org/3/person/$1/movie_credits?api_key=$API_KEY" \
+    | jq -r '.cast | sort_by(.release_date) | last(.[]) | .title'
+}
+
+get_actor_ids | while read id; do
+  name=$(get_name_by_id $id)
+  movie=$(get_latest_movie $id)
+
+  echo "$name was most recently in $movie."
 done
