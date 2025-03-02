4388d00544056795551e123e596efe6bd39484d7
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-03-02 20:12:55 +0200

    feat: create actors.sh

---
+++ api/actors.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+url="https://api.themoviedb.org"
+
+if [ -z "$API_KEY" ]; then
+  echo "API key not found."
+else
+  curl \
+    -X GET "$url/3/person/popular?api_key=$API_KEY" \
+    -H "Accept: application/json"
+fi
